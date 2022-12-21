ced13b373947bc7cde9cb7eab39d90b9160fceeb
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-21 15:19:29 +0700

    add

---
+++ src/InsertionSort/InsertionSort.java
@@ -0,0 +1,24 @@
+package InsertionSort;
+
+public class InsertionSort {
+    static int[]array = { 5,6,9,8,5,8,9,12,25,41,6,1,8,5,2};
+    public static void insertionSort(int[]array){
+        int pos, x;
+        for(int i = 1; i < array.length; i++){
+            x = array[i];
+            pos = i;
+            while(pos > 0 && x < array[pos-1]){
+                array[pos] = array[pos-1];
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        insertionSort(array);
+        for (int element:array){
+            System.out.print(element+"\t");
+        }
+    }
+}

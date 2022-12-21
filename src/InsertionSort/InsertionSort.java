f51f0c962d4e4b9e30f1490384b3046e507a9ed4
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-21 15:21:28 +0700

    Update InsertionSort.java

--- src/InsertionSort/InsertionSort.java
+++ src/InsertionSort/InsertionSort.java
@@ -3,15 +3,15 @@ package InsertionSort;
 public class InsertionSort {
     static int[]array = { 5,6,9,8,5,8,9,12,25,41,6,1,8,5,2};
     public static void insertionSort(int[]array){
-        int pos, x;
+        int pos, tempt;
         for(int i = 1; i < array.length; i++){
-            x = array[i];
+            tempt = array[i];
             pos = i;
-            while(pos > 0 && x < array[pos-1]){
+            while(pos > 0 && tempt < array[pos-1]){
                 array[pos] = array[pos-1];
                 pos--;
             }
-            array[pos] = x;
+            array[pos] = tempt;
         }
     }
 
